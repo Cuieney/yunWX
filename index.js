1c5a615be2e4381c4fed7e9b937fba1fd8df7961
cuixiaocong <cuixiaocong@xinye.com>
2023-08-21 11:17:43 +0800

    update

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ app.post("/api/receiveMessage", async (req, res) => {
 const port = process.env.PORT || 80;
 
 async function bootstrap() {
-  await initDB();
+  // await initDB();
   app.listen(port, () => {
     console.log("启动成功", port);
   });
