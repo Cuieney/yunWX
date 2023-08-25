0994d1d9a6605b0e1667a4a62ec6c7737d7a9c59
cuixiaocong <cuixiaocong@xinye.com>
2023-08-25 11:23:48 +0800

    update

--- index.js
+++ index.js
@@ -53,8 +53,8 @@ const sendMessage = (from_appid, content) => {
 }
 app.post("/api/sendMessage", async (req, res) => {
   const { from_appid, content } = req.body
-  const res = await sendMessage(from_appid, content)
-  res.send(res)
+  const result = await sendMessage(from_appid, content)
+  res.send(result)
 });
 const weblogin = (from_appid) => {
   const request = require('request')
