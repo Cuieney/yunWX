b85d0cb65a77a2b7866e580261009fede7909164
cuixiaocong <cuixiaocong@xinye.com>
2024-06-03 09:49:26 +0800

    修改url

--- src/common/const.ts
+++ src/common/const.ts
@@ -1,1 +1,3 @@
-export const WebUrl = 'https://oss.laf.run/wt5iw4-iching/index.html#/';
+// export const WebUrl = 'https://oss.laf.run/wt5iw4-iching/index.html#/';
+export const WebUrl =
+  'https://yijing-8gk8qf01dc156952-1257934448.ap-shanghai.app.tcloudbase.com/#/';
