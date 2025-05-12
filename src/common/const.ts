2d870d437e3362cbaf6d1d1aab824e32db82388d
cuixiaocong <cuixiaocong@xinye.com>
2025-05-12 20:18:41 +0800

    修改链接

--- src/common/const.ts
+++ src/common/const.ts
@@ -1,4 +1,4 @@
 // export const WebUrl = 'https://oss.laf.run/wt5iw4-wxlogin/index.html#/';
-export const WebUrl = 'https://oss.laf.dev/zzqztu-cloud-bin/index.html#/';
-// export const WebUrl =
-//   'https://yijing-8gk8qf01dc156952-1257934448.ap-shanghai.app.tcloudbase.com/#/';
+// export const WebUrl = 'https://oss.laf.dev/zzqztu-cloud-bin/index.html#/';
+export const WebUrl =
+  'https://yijing-8gk8qf01dc156952-1257934448.ap-shanghai.app.tcloudbase.com/#/';
